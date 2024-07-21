0e7a815ba554edcb1568717f7fb65a336f5b2a47
Aldrick <aldrick.kronos@gmail.com>
2024-07-21 03:13:31 -0300

    Add table classes to target-etl

---
+++ target-etl/models/signal.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, Double, DateTime
+from session_database_target import Base
+
+
+class Signal(Base):
+    __tablename__ = "signal"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
