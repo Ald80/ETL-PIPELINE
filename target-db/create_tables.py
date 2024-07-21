cf9c8638792041bb69168492fbf5cf2c57b84c4b
Aldrick <aldrick.kronos@gmail.com>
2024-07-21 02:17:02 -0300

    Delete unused files

--- target-db/create_tables.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from session_database_target import engine, Base
-from models import signal, data
-
-Base.metadata.create_all(bind=engine)
--- target-db/models/signal.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, Double, DateTime
-from session_database_target import Base
-
-class Signal(Base):
-    __tablename__ = "signal"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    
